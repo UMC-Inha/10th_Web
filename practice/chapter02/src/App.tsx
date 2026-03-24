cf342adaadee320a509651bc3d16919909257433
haewonwon <khcw1029@daum.net>
2026-03-25 04:42:57 +0900

    practice: 2주차 실습 진행 - props drilling 제거

--- practice/chapter02/src/App.tsx
+++ practice/chapter02/src/App.tsx
@@ -2,20 +2,12 @@ import ButtonGroup from './components/ButtonGroup';
 import { useCount } from './context/CounterProvider';
 
 function App() {
-  // const context = useContext(CounterContext);
-  const context = useCount();
-
-  console.log(context);
+  const { count } = useCount();
 
   return (
     <>
-      <h1>{context?.count}</h1>
-      <ButtonGroup
-        // ? 를 지워도 더이상 타입스크립트에서 에러가 발생하지 않음.
-        // 우리가 useCount 훅에서, context가 없는 경우의 에러처리를 했기 떄문.
-        handleIncrement={context.handleIncrement}
-        handleDecrement={context.handleDecrement}
-      />
+      <h1>{count}</h1>
+      <ButtonGroup />
     </>
   );
 }
--- practice/chapter02/src/components/ButtonGroup.tsx
+++ practice/chapter02/src/components/ButtonGroup.tsx
@@ -1,11 +1,8 @@
+import { useCount } from '../context/CounterProvider';
 import Button from './Button';
 
-interface ButtonGroupProps {
-  handleIncrement?: () => void;
-  handleDecrement?: () => void;
-}
-
-const ButtonGroup = ({ handleIncrement, handleDecrement }: ButtonGroupProps) => {
+const ButtonGroup = () => {
+  const { handleIncrement, handleDecrement } = useCount();
   return (
     <div>
       <Button onClick={handleIncrement} text="+1 증가" />
