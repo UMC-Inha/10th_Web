02320ee81d4db56f33932a1ae65f480c8db651eb
bukyung <boochae2003@gmail.com>
2026-05-27 23:58:13 +0900

    practice: chapter09 실습 제출

---
+++ practice/chapter09/useReducer/src/App.tsx
@@ -0,0 +1,14 @@
+//import UseReducerPage from "./pages/UseReducerPage";
+import './App.css';
+import UseReducerCompany from './pages/UseReducerCompany';
+
+function App() {
+  return (
+  <>
+    {/*<UseReducerPage/>*/}
+    <UseReducerCompany/>
+  </>
+  )
+}
+
+export default App
